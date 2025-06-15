bd9a015cdf492cc536696e3987f6d2ed8b5a725f
AuEstudo <aurelio.t.i.estudo@gmail.com>
2025-06-14 22:15:22 -0300

    Implementada edição de perfil, exclusão de anúncios e sistema de mensagens de feedback

--- apartamentos/urls.py
+++ apartamentos/urls.py
@@ -11,6 +11,7 @@ from .views import (
     PredioCreateView,
     ApartamentoCreateView,
     ApartamentoUpdateView,
+    ApartamentoDeleteView
 )
 
 app_name = 'apartamentos'
@@ -23,8 +24,15 @@ urlpatterns = [
     path('predio/novo/', PredioCreateView.as_view(), name='predio_create'),
     path('predio/<int:pk_predio>/apartamento/novo/', ApartamentoCreateView.as_view(), name='apartamento_create'),
     path('apartamento/<int:pk>/editar/', ApartamentoUpdateView.as_view(), name='apartamento_update'),
+    path('apartamento/<int:pk>/deletar/', ApartamentoDeleteView.as_view(), name='apartamento_delete'),
     path('', ApartamentoListView.as_view(), name='lista_apartamentos'),
     path('<int:pk>/', ApartamentoDetailView.as_view(), name='detalhe_apartamento'),
     path('signup', SignUpView.as_view(), name='signup'),
+    # URL de Perfil - vamos ter uma para ver e outra para editar
+    # Vamos manter a antiga por enquanto, mas apontando para uma nova view de detalhe se necessário,
+    # ou podemos simplesmente ter uma única página de edição como "página de perfil".
+    # Vamos simplificar e ter a página /meu-perfil/ como a página de EDIÇÃO.
+    path('meu-perfil/', perfil_view, name='perfil_edit'),
     path('meu-perfil/', perfil_view, name='perfil'),
+
 ]
